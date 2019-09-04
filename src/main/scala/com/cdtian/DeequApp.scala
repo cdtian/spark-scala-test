4d07165951ab05458748d3917faa832d1be2cd0f
tian yongjianckey <tianyj3@lenovo.com>
2019-09-04 14:49:32 +0800

    feature: metrics_ApproxCountDistinct

--- src/main/scala/com/cdtian/DeequApp.scala
+++ src/main/scala/com/cdtian/DeequApp.scala
@@ -1,9 +1,9 @@
 package com.cdtian
 
+import com.amazon.deequ.analyzers._
 import com.amazon.deequ.analyzers.runners.AnalyzerContext.successMetricsAsDataFrame
 import com.amazon.deequ.analyzers.runners.{AnalysisRunner, AnalyzerContext}
-import com.amazon.deequ.analyzers._
-import org.apache.spark.sql.{Encoder, Encoders, SparkSession}
+import org.apache.spark.sql.{Encoders, SparkSession}
 
 /**
   * @author tianyj3@lenovo.com
@@ -14,21 +14,28 @@ object DeequApp {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("deequ job").getOrCreate()
     val dataSet = spark.read.textFile("/tmp/tpcds-generate/10/time_dim")
-    dataSet.show(100)
     import spark.implicits._
     val newDataSet = dataSet.map(_.split("\\|")).map(attrs => Time_Dim(attrs(0).toLong, attrs(1), attrs(2).toInt, attrs(3).toInt, attrs(4).toInt, attrs(5).toInt, attrs(6), attrs(7), attrs(8), attrs(8)))(Encoders.product[Time_Dim])
     val dataDf = newDataSet.toDF()
+    val t_sub_shift_count = dataDf.groupBy("t_sub_shift").count().show()
+    val t_hour_count = dataDf.groupBy("t_hour").count().show()
+    println()
+    println(s"$t_sub_shift_count")
+    println(s"$t_hour_count")
+    println()
     val analysisResult: AnalyzerContext = {
       AnalysisRunner.onData(dataDf)
         .addAnalyzer(Size())
         .addAnalyzer(Completeness("t_time_sk"))
         .addAnalyzer(ApproxCountDistinct("t_sub_shift")) //
+        .addAnalyzer(ApproxCountDistinct("t_hour")) //
         .run()
     }
-
+    println()
     val metrics = successMetricsAsDataFrame(spark, analysisResult);
     metrics.show();
-
+    println()
+    spark.stop()
   }
 
   case class Time_Dim(
