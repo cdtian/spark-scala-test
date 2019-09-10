a20a1cd5b4ed4fbe53f11583484e2ee010b7c958
tian yongjianckey <tianyj3@lenovo.com>
2019-09-10 09:55:16 +0800

    fix

--- src/main/scala/com/cdtian/DeequApp.scala
+++ src/main/scala/com/cdtian/DeequApp.scala
@@ -12,7 +12,7 @@ import org.apache.spark.sql.{Encoders, SparkSession}
   */
 object DeequApp {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName("run 1 size 1 Completeness").getOrCreate()
+    val spark = SparkSession.builder().appName("run 1 size 2 Completeness").getOrCreate()
     val dataSet = spark.read.textFile("/tmp/tpcds-generate/10/time_dim")
     import spark.implicits._
     val newDataSet = dataSet.map(_.split("\\|")).map(attrs => Time_Dim(attrs(0).toLong, attrs(1), attrs(2).toInt, attrs(3).toInt, attrs(4).toInt, attrs(5).toInt, attrs(6), attrs(7), attrs(8), attrs(8)))(Encoders.product[Time_Dim])
@@ -21,6 +21,7 @@ object DeequApp {
       AnalysisRunner.onData(dataDf)
         .addAnalyzer(Size())
         .addAnalyzer(Completeness("t_time_sk")) //非空数据百分比
+        .addAnalyzer(Completeness("t_meal_time")) //非空数据百分比
 //        .addAnalyzer(ApproxCountDistinct("t_sub_shift")) // 统计不同数据个数(接近)
 //        .addAnalyzer(ApproxCountDistinct("t_hour")) //
 //        .addAnalyzer(ApproxCountDistinct("t_time")) //
