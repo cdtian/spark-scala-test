368809e6d12fa7a28bcbc8c0da8a59bebafd40b9
tian yongjianckey <tianyj3@lenovo.com>
2019-09-10 15:16:53 +0800

    fix

--- src/main/scala/com/cdtian/DeequApp.scala
+++ src/main/scala/com/cdtian/DeequApp.scala
@@ -76,7 +76,7 @@ object DeequApp {
 //        .addAnalyzer(MutualInformation(Seq("t_minute", "t_second")))
         .addAnalyzer(UniqueValueRatio("t_minute")) //列的不同值与列的所有值的比值
         .addAnalyzer(Distinctness("t_minute")) //列的不同值与列的所有值的比值
-//        .addAnalyzer(Uniqueness("t_hour")) //列的不同值与列的所有值的比值
+        .addAnalyzer(Uniqueness("t_hour")) //列的不同值与列的所有值的比值
 //        .addAnalyzer(Uniqueness("t_time_id")) //列的不同值与列的所有值的比值
         .run()
     }
